8410686312f02370f6ab62c213e5e930c4be8a8e
Gurpreet Singh <gurpreet.singh@affluencesolutions.co.in>
2025-02-18 02:55:43 +0530

    Add FluxService and corresponding test for reactive data flow

--- src/main/java/com/reactive/Reactive/Project/services/FluxService.java
+++ src/main/java/com/reactive/Reactive/Project/services/FluxService.java
@@ -1,4 +1,14 @@
 package com.reactive.Reactive.Project.services;
 
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
 public class FluxService {
+
+    public Flux<String> getFlux() {
+        return Flux.just("A", "B", "C").log();
+
+    }
+
 }
--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import com.reactive.Reactive.Project.services.FluxService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 public class FluxTest {
+    @Autowired
+    private FluxService fluxService;
+
+    @Test
+    public void simpleFluxTest() {
+        fluxService.getFlux().subscribe(data -> {
+            System.out.println(data);
+            System.out.println("Done with Flux Data");
+        });
+    }
 }
